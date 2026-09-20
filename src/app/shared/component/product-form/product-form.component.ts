538b1fdb8108ab36b7ab4000e089a29bfbb30f67
Hrutwikbaraskar <117747747+hrutb@users.noreply.github.com>
2026-09-20 16:36:49 +0530

    edit and update functionality is addec in this commit

--- src/app/shared/component/product-form/product-form.component.ts
+++ src/app/shared/component/product-form/product-form.component.ts
@@ -1,15 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Iproduct } from '../../model/Iproduct';
+import { ProductService } from '../../service/product.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-product-form',
   templateUrl: './product-form.component.html',
   styleUrls: ['./product-form.component.scss']
 })
-export class ProductFormComponent implements OnInit {
+export class ProductFormComponent implements OnInit{
 
-  constructor() { }
+  constructor(private _prodService:ProductService, private _snackbar:MatSnackBar) { }
+ @ViewChild('productForm') productForm!:NgForm;
+//  @Input()  emiteditObj!:Iproduct;
+
+ IsInEditMode:boolean = false ;
+ editObj!:Iproduct ;
+
+//  ngOnChanges(changes: SimpleChanges): void {
+//    this.editObj =changes['emiteditObj']['currentValue'];
+//   if(this.editObj){
+//      this.IsInEditMode =true ;
+//         this.productForm.form.patchValue(this.emiteditObj);
+//       }
+//  }
 
   ngOnInit(): void {
+
+     this._prodService.emiteditSub$
+             .subscribe({
+                   next:res=>{
+                        this.editObj = res ;
+                         this.productForm.form.patchValue(this.editObj);
+                         this.IsInEditMode = true;
+                   } ,
+                   error:err=>{
+                      console.log(err);
+                   }
+             })
+
   }
 
+
+
+  onUpdate(){
+        let updateId = this.editObj.id;
+
+        let updateObj ={
+              ...this.productForm.form.value,
+                 id:updateId
+         }
+       this._prodService.onUPdateProduct(updateObj)
+             .subscribe({
+                next:res=>{
+                    this._snackbar.open(res.msg,'close',{  duration:3000})
+
+                    this.IsInEditMode = false ;
+                    this.productForm.reset();
+                },
+                error:err=>{
+                    console.log()//
+                }
+             })
+
+  }
 }
