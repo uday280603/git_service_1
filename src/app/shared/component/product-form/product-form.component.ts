0c2add1325bc253faa873e66c2fc7e07d7f8d6e4
neerajyernale <yneerajk2003@gmail.com>
2026-09-20 16:06:43 +0530

    create functionality complted by neeraj

--- src/app/shared/component/product-form/product-form.component.ts
+++ src/app/shared/component/product-form/product-form.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ProductService } from '../../service/product.service';
+import { Iproduct } from '../../model/Iproduct';
+import { NgForm } from '@angular/forms';
+import { SnackbarServiceService } from 'src/app/snackbar-service.service';
+import { error } from 'console';
 
 @Component({
   selector: 'app-product-form',
@@ -6,10 +11,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-form.component.scss']
 })
 export class ProductFormComponent implements OnInit {
+  @ViewChild('productForm') productForm!:NgForm;
+  isInEditMode:boolean=false;
 
-  constructor() { }
+  constructor(private productService: ProductService, private snackbar:SnackbarServiceService) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    if (this.productForm.valid) {
+      const newObj = { ...this.productForm.form.value, id: crypto.randomUUID() };
+      this.productService.createProduct(newObj)
+        .subscribe({
+          next:res=>{
+            this.snackbar.sanckbar(res.msg);
+            this.productForm.reset();
+          },
+          error:err=>{
+            console.log(err);
+          }
+        })
+    }
+  }
+  onUpdate(){
+
+  }
 }
